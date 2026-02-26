6cd3dc0c35b70fe88fff7a2a002bb74477e1f159
nikel123-sketch <webdevelopernikel@gmail.com>
2026-02-26 15:15:58 +0700

    check

--- src/app/Dasbord/layout.jsx
+++ src/app/Dasbord/layout.jsx
@@ -6,7 +6,7 @@ const DashbordLayout = ({ children }) => {
   return (
     <div className="grid grid-cols-12 gap-3">
       <div className="col-span-3  border-r-1 min-h-screen">
-        <Link href={"/"} className="font-bold text-2xl ">
+        <Link href={"/"} className="font-bold text-5xl">
           Dev Story
         </Link>
         <hr />
